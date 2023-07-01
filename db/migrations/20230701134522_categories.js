29d73f1273dc78800c922806ca398f48a553e1af
Sardor <henkook-baron@mail.ru>
2023-07-01 18:49:46 +0500

    create categories migrate

---
+++ db/migrations/20230701134522_categories.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('categories', (table) => {
+    table.increments('id');
+    table.string('name', 100).notNullable().unique();
+    table.boolean('is_deleted').defaultTo(false);
+    table.timestamps(true, true)
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('categories');
+};
