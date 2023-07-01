dfc97b664ed40469b2fa7cf5a82ce306d0613a3c
Sardor <henkook-baron@mail.ru>
2023-07-01 16:58:25 +0500

    create mmiddlewares: isLoggedIn and hasRole

--- src/modules/users/_api.js
+++ src/modules/users/_api.js
@@ -1,9 +1,13 @@
+const { isLoggedIn, hasRole } = require('../../shared/auth');
 const { getUsers, getUser, loginUser } = require('./_controllers');
 
 const router = require('express').Router();
 
+const mGetUsers = [isLoggedIn, hasRole(['super_admin', 'admin'])];
+const mGetUser = [isLoggedIn, hasRole(['super_admin', 'admin'])];
+
 router.post('/users', loginUser);
-router.get('/users', getUsers);
-router.get('/users/:id', getUser);
+router.get('/users', mGetUsers, getUsers);
+router.get('/users/:id', mGetUser, getUser);
 
 module.exports = router;
---
+++ src/shared/auth/isLoggeddIn.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const { UnauthorizedError } = require('../errors');
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @param {express.NextFunction} next 
+ */
+const isLoggedIn = (req, res, next) => {
+  try {
+    const { authorization: token } = req.headers;
+
+    if (!token) {
+      throw new UnauthorizedError('Login qilmagansiz');
+    };
+
+    const decoded = jwt.verify(token, config.jwt.secret);
+
+    req.user = { id: decoded.id, role: decoded.role };
+
+    next();
+  } catch (error) {
+    err = new UnauthorizedError('Login qilmagansiz');
+    next(err);
+  };
+};
+
+module.exports = isLoggedIn;
